918e7d03d5be0cf52f9356372a1d404763e94f6a
Cole Enslen <117124101+Lumbus22@users.noreply.github.com>
2024-02-01 17:19:48 -0500

    Update DataSystemInterface.java

--- DataSystemInterface.java
+++ DataSystemInterface.java
@@ -1,13 +1,13 @@
 public interface DataSystemInterface {
     // Store data
-    Response storeData(String identifier, Data data);
+  Response storeData(String identifier, Data data);
 
     // Retrieve data
-    DataResponse retrieveData(String identifier);
+  DataResponse retrieveData(String identifier);
 
     // Update existing data
-    Response updateData(String identifier, Data newData);
+  Response updateData(String identifier, Data newData);
 
     // Delete data
-    Response deleteData(String identifier);
+  Response deleteData(String identifier);
 }
