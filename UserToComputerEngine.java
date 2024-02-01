210e2267c1a031fb6f8e9403a7864f0ee3da4a2c
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-01 12:10:47 -0500

    Update UserToComputerEngine.java

--- UserToComputerEngine.java
+++ UserToComputerEngine.java
@@ -1,10 +1,10 @@
 public interface UserToComputerEngine{
-    // Allows user to set source and destination for where input integers are read from and where output is saved to
-    Response setSourceAndDest(String sourceURL, String destinationURL);
+// Allows user to set source and destination for where input integers are read from and where output is saved to
+Response setSourceAndDest(String sourceURL, String destinationURL);
 
-    // Carries out the computation of the digit factorial, with or without default delimiter
-    Request exCompDefaultDelim(String source);
-    Request exCompCustomDelim(String delim, String source);
+// Carries out the computation of the digit factorial, with or without default delimiter
+Request exCompDefaultDelim(String source);
+Request exCompCustomDelim(String delim, String source);
 }
 
 public interface Request{
