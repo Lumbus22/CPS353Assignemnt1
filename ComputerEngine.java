c684558d4153239203aae87cef1f396d4f114f01
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-01 12:15:02 -0500

    Update ComputerEngine.java

--- ComputerEngine.java
+++ ComputerEngine.java
@@ -1,20 +1,26 @@
 public interface ComputerEngine {
 
-    //* Reading, Writing, and Initialization Component *//  
+  //* Reading, Writing, and Initialization Component *//  
 
-    // initialize a computation to be performed
-    Response initializeComputation(ComputationParameters parameters);
-    // Recieve data from user
-    Response fromUserData(int[] dataString);
-    // Send data to computation component
-    Response sendDataToCompute(Data data);
+  // initialize a computation to be performed
+  Request initializeComputation(ComputationParameters parameters);
+  // Recieve data from user
+  Request fromUserData(int[] dataString);
+  // Send data to computation component
+  Response sendDataToCompute(Data data);
 
-    //* Computation Component *//   
+  //* Computation Component *//   
     
-    // Receive data from reader component
-    Data receiveDataForComputation();
-    // Manipulate Data
-    Response performDigitFactorial();
-    // Return data to data manager
-    Response returnDigitFactorialToUserInterface(outputURL);
+  // Receive data from reader component
+  Data receiveDataForComputation();
+  // Manipulate Data
+  Request performDigitFactorial();
+  // Return data to data manager
+  Response returnDigitFactorialToUserInterface(outputURL);
+}
+public interface Request{
+    
+}
+public interface Response{
+    
 }
