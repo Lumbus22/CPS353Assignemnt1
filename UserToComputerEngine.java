4c750edd4901a25bbaee47d8d0a863445dca4563
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-01 11:17:02 -0500

    Update UserToComputerEngine.java

--- UserToComputerEngine.java
+++ UserToComputerEngine.java
@@ -1,13 +1,8 @@
 public interface UserToComputerEngine{
-    // Allows user to set source for where input integers are read from
-    Response setSource(String sourceURL);
+    // Allows user to set source and destination for where input integers are read from and where output is saved to
+    Response setSourceAndDest(String sourceURL, String destinationURL);
 
-    // Allows user to set destination for output of digit factorials
-    Response setDestination(String destinationURL);
-
-    // Either uses default delimter of , or for user to choose their own delimeter
-    Response setDelimiter(String[] delimiter);
-
-    // Carries out the computation of the digit factorial
-    Response executeComputation(Source);
+    // Carries out the computation of the digit factorial, with or without default delimiter
+    Response exCompDefaultDelim(Source);
+    Response exCompCustomDelim(String delim, Source);
 }
