7e4c6aa9099af4861a08bbc6a39eafb52250ff77
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-09 17:46:05 -0500

    Update ComputerEngine.java
    Adding empty implementation of the API

--- ComputerEngine.java
+++ ComputerEngine.java
@@ -7,12 +7,25 @@ public interface ComputerEngine {
   // Return data to data manager
   Response returnDigitFactorial(String outputURL);
 }
-public interface Request{
-    
-}
-public interface Response{
-    
-}
-public interface Data{
-  
-}
+
+
+public class ComputerEngineImpl implements ComputerEngine {
+
+    private DataSystemInterface dataStore;
+
+    @Override
+    public Data receiveDataForComputation() {
+        return null;
+    }
+
+    @Override
+    public Request performDigitFactorial() {
+        return new Request();
+    }
+
+    @Override
+    public Response returnDigitFactorial(String outputURL) {
+        return new Response();
+    }
+} 
+
