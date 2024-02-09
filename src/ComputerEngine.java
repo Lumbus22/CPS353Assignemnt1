9319d3e29b999c8719702b52d7270074d98ec854
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-09 18:00:03 -0500

    Update ComputerEngine.java (#31)
    * Update ComputerEngine.java
    
    * Update ComputerEngine.java
    
    * Update ComputerEngine.java

--- src/ComputerEngine.java
+++ src/ComputerEngine.java
@@ -1,18 +1,29 @@
 public interface ComputerEngine {
   Data receiveDataForComputation();
-  
-  // Manipulate Data
+
+    // Manipulate Data
   Request performDigitFactorial();
-  
-  // Return data to data manager
+
+    // Return data to data manager
   Response returnDigitFactorial(String outputURL);
 }
-public interface Request{
-    
-}
-public interface Response{
-    
-}
-public interface Data{
-  
+
+public class ComputerEngineImpl implements ComputerEngine {
+
+  private DataSystemInterface dataStore;
+
+  @Override
+  public Data receiveDataForComputation() {
+    return null;
+  }
+
+  @Override
+  public Request performDigitFactorial() {
+    return new Request();
+  }
+
+  @Override
+  public Response returnDigitFactorial(String outputURL) {
+    return new Response();
+  }
 }
