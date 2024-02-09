042a39e2f4ad672d730a8703fe570f258d071ce1
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-09 17:59:01 -0500

    Update ComputerEngine.java

--- src/ComputerEngine.java
+++ src/ComputerEngine.java
@@ -14,16 +14,16 @@ public class ComputerEngineImpl implements ComputerEngine {
 
   @Override
   public Data receiveDataForComputation() {
-        return null;
-    }
+    return null;
+  }
 
   @Override
   public Request performDigitFactorial() {
-        return new Request();
-    }
+    return new Request();
+  }
 
   @Override
   public Response returnDigitFactorial(String outputURL) {
-        return new Response();
-    }
+    return new Response();
+  }
 }
