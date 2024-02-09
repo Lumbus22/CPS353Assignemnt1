dbb48f4cd6c14937bc5c9524cf1ff33d19555b3a
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-09 17:32:25 -0500

    Updated methods for ComputerEngine
    Removed some unnecessary methods for this API

--- ComputerEngine.java
+++ ComputerEngine.java
@@ -1,26 +1,18 @@
 public interface ComputerEngine {
-
-  //* Reading, Writing, and Initialization Component *//  
-
-  // initialize a computation to be performed
-  Request initializeComputation(String parameters);
-  // Recieve data from user
-  Request fromUserData(int[] dataString);
-  // Send data to computation component
-  Response sendDataToCompute(int[] dataString );
-
-  //* Computation Component *//   
-    
-  // Receive data from reader component
   Data receiveDataForComputation();
+  
   // Manipulate Data
   Request performDigitFactorial();
+  
   // Return data to data manager
-  Response returnDigitFactorialToUserInterface(String outputURL);
+  Response returnDigitFactorial(String outputURL);
 }
 public interface Request{
     
 }
 public interface Response{
     
+}
+public interface Data{
+  
 }
