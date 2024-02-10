df3af71044a51640fbdf65b7e22a4d7d9142d33c
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-10 16:33:26 -0500

    Update DataSystemInterface.java

--- src/DataSystemInterface.java
+++ src/DataSystemInterface.java
@@ -11,3 +11,27 @@ public interface DataSystemInterface {
     // Delete data
   Response deleteData(String identifier);
 }
+
+public class DataSystem implements DataSystemInterface {
+
+  @Overide
+  public Response storeData(String identifier, Data data) {
+        //Send to data storage system
+        return dataLocationMaybe;
+
+    }
+
+  @Override
+  public Response updateData(String identifier, Data newData) {
+        //Updates the current data
+        return dataLocation;
+    }
+
+    // Delete data
+  @Override
+  public Response deleteData(String identifier) {
+        //Returns the deleted data
+        return deletedData;
+    }
+}
+
