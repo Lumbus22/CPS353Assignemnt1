208a1a4cf6e8ce2882a7363520980f49754e103a
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-10 18:47:45 -0500

    Update TestComputerEngine.java

--- src/test/TestComputerEngine.java
+++ src/test/TestComputerEngine.java
@@ -1,7 +1,8 @@
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 
 public class TestComputerEngine {
 
