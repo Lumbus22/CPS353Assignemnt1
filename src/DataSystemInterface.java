ee92af7b4c59a87c3d4b17570ba13b90cfb44687
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-10 16:17:05 -0500

    Revert "Delete src/DataSystemInterface.java"
    This reverts commit beb1d52432dbc1f2abd1638044c1c4ba89b4c205.

---
+++ src/DataSystemInterface.java
@@ -0,0 +1,13 @@
+public interface DataSystemInterface {
+    // Store data
+  Response storeData(String identifier, Data data);
+
+    // Retrieve data
+  DataResponse retrieveData(String identifier);
+
+    // Update existing data
+  Response updateData(String identifier, Data newData);
+
+    // Delete data
+  Response deleteData(String identifier);
+}
