58bb9bb04919ceb353ffbcf8d9d3ce1e5d3e3ab7
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-12 21:24:08 -0500

    Update checkstyle_checks.xml (#48)
    * Update checkstyle_checks.xml
    
    Removed old checkstyle for indentation and added a new one for unused imports
    
    * Update checkstyle_checks.xml
    
    * Update checkstyle_checks.xml
    
    * Update TestComputerEngine.java

--- src/test/TestComputerEngine.java
+++ src/test/TestComputerEngine.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 
 public class TestComputerEngine {
