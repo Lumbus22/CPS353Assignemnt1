ab7e9c2b1e8f5aa90d0db6f28871b89b031f1cf2
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-18 18:28:08 -0500

    Create UserToComputerEngineInterface.java

---
+++ src/JavaAPIs/Interfaces/UserToComputerEngineInterface.java
@@ -0,0 +1,10 @@
+import interfaces;
+
+public interface UserToComputerEngineInterface{
+// Allows user to set source and destination for where input integers are read from and where output is saved to
+  Response setSourceAndDest(SourceInterface sourceInterface);
+
+// Carries out the computation of the digit factorial, with or without default delimiter
+  Request exCompDefaultDelim(String source);
+  Request exCompCustomDelim(String delim, String source);
+}
