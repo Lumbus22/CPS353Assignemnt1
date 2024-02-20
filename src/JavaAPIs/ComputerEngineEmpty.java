a121b385d8454b6070a935ee6717951577cd70ce
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-20 11:24:37 -0500

    Outer file did not match inner name

--- src/JavaAPIs/ComputerEngineEmpty.java
+++ src/JavaAPIs/ComputerEngineEmpty.java
@@ -1,6 +1,6 @@
 import java.io.FileNotFoundException;
 
-public class ComputerEngineImpl implements ComputerEngineInterface {
+public class ComputerEngineEmpty implements ComputerEngineInterface {
 
     @Override
     public void receiveDataForComputation() throws FileNotFoundException {
