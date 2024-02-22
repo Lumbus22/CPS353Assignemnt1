dbffd493db86c97583de7bb670499702eb52e514
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-21 19:15:27 -0500

    Throwing IOException instead of FileNotFoundException
    For receiveDataForComputation()

--- src/JavaAPIs/ComputerEngineEmpty.java
+++ src/JavaAPIs/ComputerEngineEmpty.java
@@ -1,9 +1,9 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class ComputerEngineEmpty implements ComputerEngineInterface {
+public class ComputerEngineImpl implements ComputerEngineInterface {
 
     @Override
-    public void receiveDataForComputation() throws FileNotFoundException {
+    public void receiveDataForComputation() throws IOException {
         
     }
 
