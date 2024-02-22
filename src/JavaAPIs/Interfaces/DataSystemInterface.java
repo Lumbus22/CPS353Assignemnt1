c02e7c35d48dfd8b202001b6b871439161a9cc03
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-22 17:18:30 -0500

    Update DataSystemInterface.java
    Added and removed some methods

--- src/JavaAPIs/Interfaces/DataSystemInterface.java
+++ src/JavaAPIs/Interfaces/DataSystemInterface.java
@@ -1,9 +1,14 @@
-public interface DataSystemInterface {
-    // Store data
-    String[] writeToFile(String identifier, long[][] data);
+import java.io.IOException;
 
+public interface DataSystemInterface {
     // Retrieve data
-    String readFromFile(String identifier);
+    public void readFromFile() throws IOException;
+    
+    // Store data
+    public void writeToFile(long[][] data, String delimiter) throws IOException;
+
+    // Set output file
+    public void setDestination(String destinationFilePath);
 
     // Update existing data
     String updateData(String identifier, String[] newData);
