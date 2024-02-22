7826f3e3474780eb8ff2791be2b988eb15594eaf
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-22 17:14:28 -0500

    Update DataSystem.java
    Should be an empty class

--- src/JavaAPIs/DataSystem.java
+++ src/JavaAPIs/DataSystem.java
@@ -1,73 +1,31 @@
-
-
-import java.io.FileWriter;
 import java.io.IOException;
 
-import java.io.BufferedWriter;
-//import org.junit.Test;
-//import org.junit.*;
+public class DataSystemEmpty implements DataSystemInterface{
+    
+    // Retrieve data
+    public void readFromFile() throws IOException{
 
-
-
-public class DataSystem implements DataSystemInterface {
-
-    //this is to test the file creation and output
-
-    public static void main(String[] args) {
-        long[][] array = new long[2][2];
-        DataSystem dataSystem = new DataSystem();
-        String[] data = dataSystem.writeToFile("testIdentifier", array);
-        System.out.println(data);
-        String readData = dataSystem.readFromFile("test");
-        System.out.println(readData);
-        String updatedData = dataSystem.updateData("test", new String[]{"This is an updated test"});
-        System.out.println(updatedData);
-        String deletedData = dataSystem.deleteData("test");
-        System.out.println(deletedData);
     }
     
-  
-  @Override
-  public String[] writeToFile(String identifier, long[][] data) {
+    // Store data
+    public void writeToFile(long[][] data, String delimiter) throws IOException{
 
-      String[] returnString = new String[4];
-  
-      // Saves data to output.txt
-      try {
-          BufferedWriter writer = new BufferedWriter(new FileWriter("dataStorage\\" + identifier + ".txt"));
-          writer.write(identifier + "\n");
-          for (int i = 0; i < data.length; i++) {
-              for (int j = 0; j < data[i].length; j++) {
-                  writer.write(data[i][j] + ", ");
-              }
-              writer.write("\n");
-          }
-          
-          writer.close();
-  
-          System.out.println(data + ", Was written to file output.txt");
-          } catch(IOException e) { 
-              e.printStackTrace();
-          }
-    } // Add closing curly brace here
-      
-    @Override
-    public String readFromFile(String identifier){
-        // Retrieve data
-        return identifier;//RetrievedData;
     }
-    
-  @Override
-  public String updateData(String identifier, String[] newData) {
-        //Updates the current data
+
+
+    // Set output file
+    public void setDestination(String destinationFilePath){
+
+    }
+
+    // Update existing data
+    public String updateData(String identifier, String[] newData){
         return identifier;
     }
 
     // Delete data
-  @Override
-  public String deleteData(String identifier) {
-        //Returns the deleted data
+    public String deleteData(String identifier){
         return identifier;
+
     }
 }
-
