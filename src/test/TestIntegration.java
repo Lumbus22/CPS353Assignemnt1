fde14a24235a3791c1f5eba4c3c1352a27023bca
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-04-03 22:03:54 -0400

    Changed Integration Test

--- src/test/TestIntegration.java
+++ src/test/TestIntegration.java
@@ -1,24 +1,26 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
 
 public class TestIntegration {
-    
+
     @Test
-    public void testStartComputation() {
-        DataSystem dataSystem = new DataSystem("/test/dataTests/inputtests.csv", "/test/dataTests/testoutput.csv");
-        ComputationImpl computation = new ComputationImpl("/test/dataTests/inputtests.csv");
+    public void testStartComputation() throws IOException {
+        String filePath = "source/to/file";
+        DataSystem testDataSystem = new DataSystem("/test/dataTests/inputtests.csv", "/test/dataTests/testoutput.csv");
+        
+        ComputationImpl computerEngine = new ComputationImpl(filePath);
+        computerEngine.setDataSystem(testDataSystem);
+        
+        CoordinatorImpl coordinator = new CoordinatorImpl(testDataSystem);
 
-        CoordinatorImpl coordinator = new CoordinatorImpl(dataSystem);
-        String sourceFilePath = "/test/dataTests/inputtests.csv";
-        coordinator.setSource(sourceFilePath);
-        String destinationFilePath = "/test/dataTests/testoutput.csv";
-        boolean isSuccess = coordinator.startComputation(destinationFilePath);
-        if (isSuccess) {
-            System.out.println("Computation completed successfully and results are written to " + destinationFilePath);
-        } else {
-            System.err.println("Computation failed.");
-        }
-        assertEquals(true, isSuccess);
+        computerEngine.receiveDataForComputation(); 
+        long[][] computationResult = computerEngine.performDigitFactorial();
+        
+        boolean userToComputerResult = coordinator.startComputation("destination/path");
+
+        assertNotNull(computationResult, "Computation result should not be null");
+
+        assertTrue(userToComputerResult, "Processing user request failed");
     }
-
 }
