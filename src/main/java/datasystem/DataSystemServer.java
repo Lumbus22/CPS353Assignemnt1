0578c1cace226b8d079b4fa0bc2f252c3d387ea0
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-04-09 21:28:00 -0400

    Added webapp

---
+++ src/main/java/datasystem/DataSystemServer.java
@@ -0,0 +1,45 @@
+package datasystem;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+
+public class DataSystemServer {
+
+    private Server server;
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50058;
+        server = ServerBuilder.forPort(port)
+                .addService(new DataSystemImpl())
+                .build()
+                .start();
+
+        System.out.println("Server started, listening on " + port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            DataSystemServer.this.stop();
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final DataSystemServer server = new DataSystemServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+}
